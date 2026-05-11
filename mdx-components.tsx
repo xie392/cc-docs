0471e9815be36841583ee76da85cf6b2f124020f
xie392 <ymzdjiang@qq.com>
2026-05-11 15:16:25 +0800

    fix: add MDXComponents type annotation for build

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 import { useMDXComponents as getDocsMDXComponents } from "nextra-theme-docs";
+import type { MDXComponents } from "nextra/mdx-components";
 
-export function useMDXComponents(components) {
+export function useMDXComponents(components: MDXComponents) {
   return {
     ...getDocsMDXComponents(components),
     ...components,
